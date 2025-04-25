55787a4757e4aee3c5e52e9ecc4159374fcb240d
tahleelprodhan <tahleel@gmail.com>
2025-04-25 08:56:14 -0400

    Initial commit

---
+++ AppleFoundationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFoundationsApp.swift
+//  AppleFoundations
+//
+//  Created by Macbook on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleFoundationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
